0413c61e99b47314553c4945c787c5be8248dfa8
maisam2004 <maisam_2004@hotmail.com>
2023-11-04 12:47:09 +0000

    Add fist.js using fetch api

---
+++ fetch/first.js
@@ -0,0 +1,91 @@
+const apiUrl = "https://ci-swapi.herokuapp.com/api/films/";
+const urlBase = "https://ci-swapi.herokuapp.com/api/";
+
+const dataContainer = document.getElementById('data-container');
+
+fetch(apiUrl)
+    .then(Response => {
+        if(!Response.ok){
+            throw new Error('Network response was not ok');
+        }
+        return Response.json();
+    })
+    .then(data => {
+    
+
+        dataContainer.innerHTML = '<ul>';
+        console.dir(data);
+
+        for( fi of data.results) {
+            dataContainer.innerHTML += `<li>${fi.title}</li>`;
+        }
+
+        dataContainer.innerHTML += `<li>${data.results}</li>`;
+       
+        dataContainer.innerHTML +='</ul>'
+    })
+    .catch(error => {
+        console.error('Fetch error',error);
+        dataContainer.innerHTML = 'An error occured while fetching data.';
+    });
+
+// Create a reusable function for making fetch requests
+function extraLink(link){
+    let url = "https://ci-swapi.herokuapp.com/api/"+link;
+        return fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+        
+                let listHtml= '<ol>';
+                data.results.forEach(element => {
+                    
+                    listHtml += `<li>${element.name}</li>`;
+                })
+                listHtml += '</ol>';
+                document.getElementById('sp').innerHTML = listHtml;
+                
+            })
+            .catch(error =>{
+                console.error('Error:', error);
+            });         
+        
+    }
+
+const specific = document.getElementById('sp') ;
+const planets = document.getElementById('planets') ;
+
+/* 
+extraLink('people/');
+    .then(data => {
+        
+        let listHtml= '<ol>';
+        data.results.forEach(element => {
+            
+            listHtml += `<li>${element.name}</li>`;
+        })
+        listHtml += '</ol>';
+        specific.innerHTML = listHtml;
+        
+    }); */
+
+ /*
+ extraLink('planets/')
+   .then(data => {
+        
+        let listHtml= '<ol>';
+        data.results.forEach(element => {
+            listHtml += `<li>${element.name}</li>`;
+        })
+        listHtml += '</ol>';
+        planets.innerHTML = listHtml;
+        
+    }); */
+
+
+
+
